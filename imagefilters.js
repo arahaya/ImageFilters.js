265c80b1355b952dddfcca4b456deb6f53d863bd
arahaya <arahaya.com@gmail.com>
2011-06-10 13:53:02 +0900

    ADD Dither and CloneBuiltin, DELETE BasicBlur because it's slow and not effective

--- imagefilters.js
+++ imagefilters.js
@@ -36,7 +36,7 @@ ImageFilters.utils = {
     },
     buildMap: function (f) {
         for (var m = [], k = 0, v; k < 256; k += 1) {
-            m[k] = (v = f(k)) > 255 ? 255 : v < 0 ? 0 : v;
+            m[k] = (v = f(k)) > 255 ? 255 : v < 0 ? 0 : v | 0;
         }
         return m;
     },
@@ -395,14 +395,6 @@ ImageFilters.Binarize = function (srcImageData, threshold) {
     }
 
     return dstImageData;
-};
-
-ImageFilters.BasicBlur = function (srcImageData) {
-    return this.ConvolutionFilter(srcImageData, 3, 3, [
-        0, 1, 0,
-        1, 1, 1,
-        0, 1, 0
-    ], 5, 0, false);
 };
 
 ImageFilters.BlendAdd = function (srcImageData, blendImageData, dx, dy) {
@@ -1028,6 +1020,28 @@ ImageFilters.Clone = function (srcImageData) {
     return this.Copy(srcImageData, this.utils.createImageData(srcImageData.width, srcImageData.height));
 };
 
+/**
+ * slower
+ */
+ImageFilters.CloneBuiltin = function (srcImageData) {
+    var srcWidth  = srcImageData.width,
+        srcHeight = srcImageData.height,
+        canvas    = this.utils.getSampleCanvas(),
+        context   = this.utils.getSampleContext(),
+        dstImageData;
+
+    canvas.width  = srcWidth;
+    canvas.height = srcHeight;
+
+    context.putImageData(srcImageData, 0, 0);
+    dstImageData = context.getImageData(0, 0, srcWidth, srcHeight);
+
+    canvas.width = 0;
+    canvas.height = 0;
+
+    return dstImageData;
+};
+
 ImageFilters.ColorMatrixFilter = function (srcImageData, matrix) {
     var srcPixels    = srcImageData.data,
         srcWidth     = srcImageData.width,
@@ -1246,6 +1260,135 @@ ImageFilters.DisplacementMapFilter = function (srcImageData, mapImageData, mapX,
     return dstImageData;
 };
 
+/**
+ * Floyd-Steinberg algorithm
+ * @param levels 2 <= n <= 255
+ */
+ImageFilters.Dither = function (srcImageData, levels) {
+    var srcWidth     = srcImageData.width,
+        srcHeight    = srcImageData.height,
+        dstImageData = this.Clone(srcImageData),
+        dstPixels    = dstImageData.data;
+
+    levels = levels < 2 ? 2 : levels > 255 ? 255 : levels;
+
+    // Build a color map using the same algorithm as the posterize filter.
+    var posterize,
+        levelMap = [],
+        levelsMinus1 = levels - 1,
+        j = 0,
+        k = 0,
+        i;
+
+    for (i = 0; i < levels; i += 1) {
+        levelMap[i] = (255 * i) / levelsMinus1;
+    }
+
+    posterize = this.utils.buildMap(function (value) {
+        var ret = levelMap[j];
+
+        k += levels;
+
+        if (k > 255) {
+            k -= 255;
+            j += 1;
+        }
+
+        return ret;
+    });
+    
+    // Apply the dithering algorithm to each pixel
+    var x, y,
+        index,
+        old_r, old_g, old_b,
+        new_r, new_g, new_b,
+        err_r, err_g, err_b,
+        nbr_r, nbr_g, nbr_b,
+        srcWidthMinus1 = srcWidth - 1,
+        srcHeightMinus1 = srcHeight - 1,
+        A = 7 / 16,
+        B = 3 / 16,
+        C = 5 / 16,
+        D = 1 / 16;
+    
+    for (y = 0; y < srcHeight; y += 1) {
+        for (x = 0; x < srcWidth; x += 1) {
+            // Get the current pixel.
+            index = (y * srcWidth + x) << 2;
+            
+            old_r = dstPixels[index];
+            old_g = dstPixels[index + 1];
+            old_b = dstPixels[index + 2];
+            
+            // Quantize using the color map
+            new_r = posterize[old_r];
+            new_g = posterize[old_g];
+            new_b = posterize[old_b];
+            
+            // Set the current pixel.
+            dstPixels[index]     = new_r;
+            dstPixels[index + 1] = new_g;
+            dstPixels[index + 2] = new_b;
+            
+            // Quantization errors
+            err_r = old_r - new_r;
+            err_g = old_g - new_g;
+            err_b = old_b - new_b;
+            
+            // Apply the matrix.
+            // x + 1, y
+            index += 1 << 2;
+            if (x < srcWidthMinus1) {
+                nbr_r = dstPixels[index]     + A * err_r;
+                nbr_g = dstPixels[index + 1] + A * err_g;
+                nbr_b = dstPixels[index + 2] + A * err_b;
+                
+                dstPixels[index]     = nbr_r > 255 ? 255 : nbr_r < 0 ? 0 : nbr_r | 0;
+                dstPixels[index + 1] = nbr_g > 255 ? 255 : nbr_g < 0 ? 0 : nbr_g | 0;
+                dstPixels[index + 2] = nbr_b > 255 ? 255 : nbr_b < 0 ? 0 : nbr_b | 0;
+            }
+            
+            // x - 1, y + 1
+            index += (srcWidth - 2) << 2;
+            if (x > 0 && y < srcHeightMinus1) {
+                nbr_r = dstPixels[index]     + B * err_r;
+                nbr_g = dstPixels[index + 1] + B * err_g;
+                nbr_b = dstPixels[index + 2] + B * err_b;
+                
+                dstPixels[index]     = nbr_r > 255 ? 255 : nbr_r < 0 ? 0 : nbr_r | 0;
+                dstPixels[index + 1] = nbr_g > 255 ? 255 : nbr_g < 0 ? 0 : nbr_g | 0;
+                dstPixels[index + 2] = nbr_b > 255 ? 255 : nbr_b < 0 ? 0 : nbr_b | 0;
+            }
+            
+            // x, y + 1
+            index += 1 << 2;
+            if (y < srcHeightMinus1) {
+                nbr_r = dstPixels[index]     + C * err_r;
+                nbr_g = dstPixels[index + 1] + C * err_g;
+                nbr_b = dstPixels[index + 2] + C * err_b;
+                
+                dstPixels[index]     = nbr_r > 255 ? 255 : nbr_r < 0 ? 0 : nbr_r | 0;
+                dstPixels[index + 1] = nbr_g > 255 ? 255 : nbr_g < 0 ? 0 : nbr_g | 0;
+                dstPixels[index + 2] = nbr_b > 255 ? 255 : nbr_b < 0 ? 0 : nbr_b | 0;
+            }
+            
+            // x + 1, y + 1
+            index += 1 << 2;
+            if (x < srcWidthMinus1 && y < srcHeightMinus1) {
+                nbr_r = dstPixels[index]     + D * err_r;
+                nbr_g = dstPixels[index + 1] + D * err_g;
+                nbr_b = dstPixels[index + 2] + D * err_b;
+                
+                dstPixels[index]     = nbr_r > 255 ? 255 : nbr_r < 0 ? 0 : nbr_r | 0;
+                dstPixels[index + 1] = nbr_g > 255 ? 255 : nbr_g < 0 ? 0 : nbr_g | 0;
+                dstPixels[index + 2] = nbr_b > 255 ? 255 : nbr_b < 0 ? 0 : nbr_b | 0;
+            }
+        }
+    }
+
+    return dstImageData;
+};
+
 ImageFilters.Edge = function (srcImageData) {
     //pretty close to Fireworks 'Find Edges' effect
     return this.ConvolutionFilter(srcImageData, 3, 3, [
@@ -1327,6 +1470,7 @@ ImageFilters.GrayScale = function (srcImageData) {
 
     for (var i = 0; i < srcLength; i += 4) {
         var intensity = (srcPixels[i] * 19595 + srcPixels[i + 1] * 38470 + srcPixels[i + 2] * 7471) >> 16;
+        //var intensity = (srcPixels[i] * 0.3086 + srcPixels[i + 1] * 0.6094 + srcPixels[i + 2] * 0.0820) | 0;
         dstPixels[i] = dstPixels[i + 1] = dstPixels[i + 2] = intensity;
         dstPixels[i + 3] = srcPixels[i + 3];
     }
@@ -1599,18 +1743,18 @@ ImageFilters.Posterize = function (srcImageData, levels) {
 
     levels = levels < 2 ? 2 : levels > 255 ? 255 : levels;
 
-    var map = [],
-        levelMinus1 = level - 1,
+    var levelMap = [],
+        levelsMinus1 = levels - 1,
         j = 0,
         k = 0,
         i;
 
     for (i = 0; i < levels; i += 1) {
-        map[i] = ((255 * i) / levelMinus1) & 0xFF;
+        levelMap[i] = (255 * i) / levelsMinus1;
     }
 
     this.utils.mapRGB(srcPixels, dstPixels, function (value) {
-        var ret = map[j];
+        var ret = levelMap[j];
 
         k += levels;
 
@@ -1714,7 +1858,8 @@ ImageFilters.ResizeBuiltin = function (srcImageData, width, height) {
     var srcWidth     = srcImageData.width,
         srcHeight    = srcImageData.height,
         canvas    = this.utils.getSampleCanvas(),
-        context   = this.utils.getSampleContext();;
+        context   = this.utils.getSampleContext(),
+        dstImageData;
 
     canvas.width  = Math.max(srcWidth, width);
     canvas.height = Math.max(srcHeight, height);
@@ -1724,13 +1869,13 @@ ImageFilters.ResizeBuiltin = function (srcImageData, width, height) {
     context.scale(width / srcWidth, height / srcHeight);
     context.drawImage(canvas, 0, 0);
 
-    var result = context.getImageData(0, 0, width, height);
+    dstImageData = context.getImageData(0, 0, width, height);
 
     context.restore();
     canvas.width = 0;
     canvas.height = 0;
 
-    return result;
+    return dstImageData;
 };
 
 ImageFilters.Sepia = function (srcImageData) {
